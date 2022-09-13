9174448c262342d815eaee4f35872c87ca4d9bb3
raturidrc <gaurav.raturi@drcsystems.com>
2022-09-13 15:38:34 +0530

    commit all files

---
+++ src/main/java/calc/Addition.java
@@ -0,0 +1,12 @@
+package calc;
+
+public class Addition {
+    public static void main(String[] args) {
+
+        int x=1;
+        int y= 20;
+
+        int sum = x+y;
+        System.out.println("Sum is :" + sum);
+    }
+}
