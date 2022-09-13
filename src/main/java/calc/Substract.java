dc98f9e87e1a7a79375fb4deede58684823c5548
raturidrc <gaurav.raturi@drcsystems.com>
2022-09-13 15:48:45 +0530

    pushing substraction.java to git

---
+++ src/main/java/calc/Substract.java
@@ -0,0 +1,11 @@
+package calc;
+
+public class Substract {
+    int x ;
+    int y ;
+
+    Substract(int x, int y){
+        int c = x-y;
+        System.out.println("substraction of two number is: " + c);
+    }
+}
